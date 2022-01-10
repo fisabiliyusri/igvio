95c9dae455c472ffd64194fc3231faed79730f4b
yasin-ai <73063465+yasin-ai@users.noreply.github.com>
2022-01-10 19:33:43 +0530

    Update Reels.jsx

--- src/components/Reels.jsx
+++ src/components/Reels.jsx
@@ -37,12 +37,13 @@ const Reels = (props) => {
             return;
         }
         const response = await fetch(
-            `https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/media_by_id?shortcode=${reelId}&response_type=reels&corsEnabled=true`,
+            `https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/media_by_id?shortcode=${reelId}&response_type=reels&corsEnabled=false`,
             {
                 method: "GET",
                 headers: {
                     "x-rapidapi-key": "d57e68a8eamsh96aa54d1038b6ffp17cb2djsn17a540de17a9",
                     "x-rapidapi-host": "instagram-bulk-profile-scrapper.p.rapidapi.com",
+                    
                 },
             }
         );
