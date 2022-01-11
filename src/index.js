b28d744ca3948261fa75cc5cde7ca71c6853c79e
yasin-ai <73063465+yasin-ai@users.noreply.github.com>
2022-01-11 18:54:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
+
         <App />
-    </BrowserRouter>,
+
+    </HashRouter>,
     document.getElementById("root")
 );
